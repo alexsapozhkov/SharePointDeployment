6922495b399708691c516e5a9e36348c2bd28c3a
Aleks <aleksandr.sapozhkov@soprasteria.com>
2022-02-22 23:15:36 +0100

    installing prerequisites via ps1

---
+++ InstallPrerequisites.ps1
@@ -0,0 +1,26 @@
+# requires -runAs
+
+####### FUNCTIONS #######
+function Ensure-Module
+{
+    param
+    (
+        [Parameter(Mandatory = $true)]
+        [System.String]
+        $Name
+    )
+
+    $moduleInGallery = Find-Module -Name $Name
+    $moduleOnServer = Get-InstalledModule $Name
+
+    # If AutomatedLab doesn't exist or version is lower than the version in the PS Gallery
+    if ($null -eq $moduleOnServer -or $moduleInGallery.Version -lt $moduleOnServer.Version)
+    {
+        Install-Module -Name $Name -Force
+    }
+}
+
+####### START SCRIPT #######
+
+Ensure-Module -Name Az
+Ensure-Module -Name AutomatedLab
