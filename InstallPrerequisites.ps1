9decdc9b0641d37860829864fa01b5e8df1d33b5
Aleks <aleksandr.sapozhkov@soprasteria.com>
2022-02-23 19:17:27 +0100

    Installing AutomatedLab 5.40.0

--- InstallPrerequisites.ps1
+++ InstallPrerequisites.ps1
@@ -1,27 +1,2 @@
-# requires -runAs
-
-####### FUNCTIONS #######
-function Ensure-Module
-{
-    param
-    (
-        [Parameter(Mandatory = $true)]
-        [System.String]
-        $Name
-    )
-
-    $moduleInGallery = Find-Module -Name $Name
-    $moduleOnServer = Get-InstalledModule $Name -ErrorAction Ignore
-
-    # If AutomatedLab doesn't exist or version is lower than the version in the PS Gallery
-    if ($null -eq $moduleOnServer -or $moduleInGallery.Version -lt $moduleOnServer.Version)
-    {
-        Install-Module -Name $Name -Force -SkipPublisherCheck -AllowClobber
-    }
-}
-
-####### START SCRIPT #######
-
-Ensure-Module -Name Az
-Uninstall-AzureRm
-Ensure-Module -Name AutomatedLab
+Install-Module -Name Az -Force -AllowClobber
+Install-Module -Name AutomatedLab -Force -SkipPublisherCheck -AllowClobber -RequiredVersion 5.41.0
