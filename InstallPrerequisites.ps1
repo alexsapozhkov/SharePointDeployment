59de77e7ef9e3b313794505e06a2928f2ebe708b
Aleks <aleksandr.sapozhkov@soprasteria.com>
2022-02-23 22:25:04 +0100

    $PSVersionTable

--- InstallPrerequisites.ps1
+++ InstallPrerequisites.ps1
@@ -1,4 +1,3 @@
-Write-host $PSVersionTable.PSEdition
-[Environment]::UserInteractive
+$PSVersionTable
 Install-Module -Name Az -Force -AllowClobber
 Install-Module -Name AutomatedLab -Force -SkipPublisherCheck -AllowClobber -RequiredVersion 5.40.0
